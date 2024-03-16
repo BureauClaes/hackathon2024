27b8336e3826f5b4b7d9a11741867ab2fbc62d72
AKIS-STUDIO <133231605+AKIS-STUDIO@users.noreply.github.com>
2024-03-16 16:06:41 +0100

    Fetch API + styling

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,1 +1,26 @@
-<footer class="bg-gray-20"></footer>
+<script>
+	let year = 2000;
+</script>
+
+<footer class="bg-neutral-800 text-white mt-12 rounded-t-cards py-8 px-4">
+	<a href="/" class="font-bold text-3xl"> dat.</a>
+	<h3>Thématiques</h3>
+	<ul class="">
+		<li><a href="">Politique</a></li>
+		<li><a href="">Économie</a></li>
+		<li><a href="">Culture</a></li>
+		<li><a href="">Santé</a></li>
+		<li><a href="">Environnement</a></li>
+	</ul>
+	<p>© {year} dat. All rights reserved.</p>
+</footer>
+
+<style>
+	h3 {
+		@apply text-3xl;
+	}
+
+	a {
+		@apply hover:opacity-80 transition-opacity;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,41 @@
 <script>
+	import { clsx } from 'clsx';
+
+	let items;
+
+	// https://dummyapi.online/api/todos
+	fetch('https://dummyapi.online/api/todos')
+		.then((response) => response.json())
+		.then((json) => (items = json));
+
+	$: console.log(items);
 </script>
 
 <h1 class="text-3xl font-bold underline">Hello world!</h1>
 <section class="px-2">
 	<ul class="grid grid-cols-2 w-full gap-4">
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
+		{#if items}
+			{#each items as item}
+				<li class="">
+					{item.title}
+					<p class="text-sm tag-{item.priority} w-fit px-4 rounded-full">{item.priority}</p>
+				</li>{/each}
+		{/if}
 	</ul>
 </section>
 
 <style lang="postcss">
 	li {
 		@apply aspect-square rounded-cards bg-slate-300;
+	}
+
+	.tag-low {
+		@apply bg-green-500;
+	}
+	.tag-medium {
+		@apply bg-yellow-300;
+	}
+	.tag-high {
+		@apply bg-red-500;
 	}
 </style>
