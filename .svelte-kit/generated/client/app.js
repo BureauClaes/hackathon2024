07a28ad080f18fede00859f2c7640029861a26fe
Steven <79700556+Ryypin@users.noreply.github.com>
2024-03-17 01:14:50 +0100

    Fetch to API

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -4,14 +4,16 @@ export const nodes = [
 	() => import('./nodes/0'),
 	() => import('./nodes/1'),
 	() => import('./nodes/2'),
-	() => import('./nodes/3')
+	() => import('./nodes/3'),
+	() => import('./nodes/4')
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
 		"/": [2],
-		"/fetch": [3]
+		"/api": [~3],
+		"/fetch": [4]
 	};
 
 export const hooks = {
---
+++ src/routes/api/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let form;
+
+	$: console.log(form);
+</script>
+
+<section class="px-2 pt-6">
+	<div class="w-fit">
+		<div id="responseBlock"></div>
+		<form method="post" action="#">
+			<div class="flex flex-col items-start mb-3">
+				<label for="question" class="mb-2">Posez votre question :</label>
+				<textarea
+					class="bg-[#F0F0F0] rounded-xl"
+					id="question"
+					name="question"
+					rows="4"
+					cols="50"
+					required
+				></textarea>
+			</div>
+			<div class="flex flex-col items-end">
+				<input
+					value="Envoyer"
+					type="submit"
+					id="submitQuestion"
+					class="px-5 py-2 bg-primary rounded-xl font-bold duration-300 hover:bg-primary-hover"
+				/>
+			</div>
+		</form>
+	</div>
+</section>
